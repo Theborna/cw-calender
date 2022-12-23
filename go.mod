08cb7c2e20e969776f11afabbd80874cff6b1ece
Theborna <borna710@gmail.com>
2022-12-23 17:15:09 +0330

    remake using golang

---
+++ go.mod
@@ -0,0 +1,7 @@
+module cw-cal
+
+go 1.19
+
+require gopkg.in/telebot.v3 v3.1.2
+
+require github.com/joho/godotenv v1.4.0 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+		panic("Error loading .env file")
+	}
+	pref := tele.Settings{
+		Token:  os.Getenv("API_KEY"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/hello", func(c tele.Context) error {
+		return c.Send("Hello!")
+	})
+
+	log.Println("Started bot successfully")
+	b.Start()
+}
