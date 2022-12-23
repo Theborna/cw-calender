3ee84ccaa8d247abc4367d891bb113dacecc6879
Theborna <borna710@gmail.com>
2022-12-23 18:55:51 +0330

    added register command

--- api/api.go
+++ api/api.go
@@ -1,10 +1,20 @@
 package api
 
 import (
+	"cw-cal/model"
+	"fmt"
 	"log"
 
 	tele "gopkg.in/telebot.v3"
 	"gopkg.in/telebot.v3/middleware"
+)
+
+const (
+	UNREGISTERED = `user @%s __%d__ is not registered on this service
+	please register using /register`
+	REGISTERED = `user @%s __%d__ is already registered on this service
+	use /deadline to check deadlines
+	or /updateUrl to update your calendar url`
 )
 
 func errHandler(err error) {
@@ -17,15 +27,15 @@ func errHandler(err error) {
 type CwBot struct {
 	*tele.Bot
 
-	name string
+	users []model.User // TODO: use database
 }
 
 func NewBot(pref tele.Settings) *CwBot {
 	b, err := tele.NewBot(pref)
 	errHandler(err)
 	Bot := &CwBot{
-		Bot:  b,
-		name: "hiii",
+		Bot:   b,
+		users: []model.User{},
 	}
 	Bot.handlers()
 	return Bot
@@ -33,21 +43,53 @@ func NewBot(pref tele.Settings) *CwBot {
 
 func (b *CwBot) handlers() {
 	b.Handle("/deadline", b.deadlineHandler(), middleware.IgnoreVia(), b.registeredMW())
+	b.Handle("/register", b.registerHandler(), middleware.IgnoreVia(), b.unregisteredMW())
 }
 
 func (b *CwBot) deadlineHandler() tele.HandlerFunc {
-	return func(c tele.Context) error {
-		return c.Send("lol")
+	return func(ctx tele.Context) error {
+		return ctx.Send("lol")
+	}
+}
+
+func (b *CwBot) registerHandler() tele.HandlerFunc {
+	return func(ctx tele.Context) error {
+		user := model.User{
+			User: ctx.Sender(),
+		}
+		// TODO: use database
+		b.users = append(b.users, user)
+		return ctx.Send("register successful")
 	}
 }
 
 func (b *CwBot) registeredMW() tele.MiddlewareFunc {
 	return func(next tele.HandlerFunc) tele.HandlerFunc {
-		return func(c tele.Context) error {
-			if c.Callback() != nil {
-				defer c.Respond()
+		return func(ctx tele.Context) error {
+			if b.isRegistered(ctx.Sender()) {
+				return next(ctx)
 			}
-			return next(c)
+			return ctx.Send(fmt.Sprintf(UNREGISTERED, ctx.Sender().Username, ctx.Sender().ID))
 		}
 	}
+}
+
+func (b *CwBot) unregisteredMW() tele.MiddlewareFunc {
+	return func(next tele.HandlerFunc) tele.HandlerFunc {
+		return func(ctx tele.Context) error {
+			if b.isRegistered(ctx.Sender()) {
+				return ctx.Send(fmt.Sprintf(REGISTERED, ctx.Sender().Username, ctx.Sender().ID))
+			}
+			return next(ctx)
+		}
+	}
+}
+
+func (b *CwBot) isRegistered(sender *tele.User) bool { // TODO: use database
+	for _, user := range b.users {
+		if user.ID == sender.ID {
+			return true
+		}
+	}
+	return false
 }
---
+++ model/model.go
@@ -0,0 +1,9 @@
+package model
+
+import (
+	tele "gopkg.in/telebot.v3"
+)
+
+type User struct {
+	*tele.User
+}
